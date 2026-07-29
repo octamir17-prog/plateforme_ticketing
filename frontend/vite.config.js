d5b64e54045c1e860521174142e9133fa64c7aa0
elmira008 <elmiragnansounou452@gmail.com>
2026-07-29 16:54:32 +0200

    best

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    watch: {
+      ignored: ['**/backend/**']
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
