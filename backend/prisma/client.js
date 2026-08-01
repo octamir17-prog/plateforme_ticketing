f9f350a98fbcf450934d1c1cedb6f187ed9c4643
unknown <amaterasu0727>
2026-08-01 17:49:40 +0200

    tcho

--- backend/prisma/client.js
+++ backend/prisma/client.js
@@ -1,7 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
-const { PrismaMariaDb } = require('@prisma/adapter-mariadb');
-
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
+const { PrismaPg } = require('@prisma/adapter-pg');
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
